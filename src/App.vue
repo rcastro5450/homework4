c14643e3b688906dac17c95cbda03de94344b04e
Roel Castro <rcastro_5450@stu.southtexascollege.edu>
2025-02-16 18:13:31 -0600

    finished Balance

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import {ref, computed} from 'vue'
+
+  const transactions = ref([
+    {id: 1, text:'Paycheck', amount: 700.00},
+    {id: 2, text:'Water Bill', amount: -72.83},
+    {id: 3, text:'Electric Bill', amount: -153.89},
+    {id: 4, text:'Returned Item', amount: 20.00},
+  ])
+
+  const sum = computed(()=>{
+    return transactions.value.reduce((acc,  x)=>{
+      return acc+x.amount
+    },0)
+  })
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <Header></Header>
+  <div class="container">
+    <Balance :total="sum"></Balance>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</template>
